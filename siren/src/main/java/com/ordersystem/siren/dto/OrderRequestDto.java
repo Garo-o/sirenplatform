f2cc9fcae99ceff518724d8424c930de2c180c63
Garo-o <78212696+Garo-o@users.noreply.github.com>
2021-11-02 20:32:16 +0900

    featrue/orders-sample

--- siren/src/main/java/com/ordersystem/siren/dto/OrderRequestDto.java
+++ siren/src/main/java/com/ordersystem/siren/dto/OrderRequestDto.java
@@ -1,2 +1,22 @@
-package com.ordersystem.siren.dto;public class OrderRequestDto {
+package com.ordersystem.siren.dto;
+
+import lombok.Getter;
+
+import java.util.List;
+
+public class OrderRequestDto {
+    @Getter
+    public static class CreateOrder{
+        private Long cafeId;
+        private Long branchId;
+        private List<MenuDto> menuDtos;
+        private String memo;
+    }
+    
+    @Getter
+    public static class MenuDto{
+        private Long menuId;
+        private Long count;
+        private Long price;
+    }
 }
--- siren/src/main/java/com/ordersystem/siren/service/OrderService.java
+++ siren/src/main/java/com/ordersystem/siren/service/OrderService.java
@@ -1,15 +1,73 @@
 package com.ordersystem.siren.service;
 
+import com.ordersystem.siren.domain.*;
+import com.ordersystem.siren.dto.OrderRequestDto;
+import com.ordersystem.siren.exception.MenuNotFoundException;
+import com.ordersystem.siren.exception.OrderNotFoundException;
+import com.ordersystem.siren.repository.CafeRepository;
+import com.ordersystem.siren.repository.MenuRepository;
 import com.ordersystem.siren.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 @Transactional(readOnly = true)
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final CafeService cafeService;
+
+    @Transactional
+    public Order createOrder(OrderRequestDto.CreateOrder createOrder, User user){
+        Map<Long, Menu> menus = getOrderMenus(createOrder);
+
+        List<OrderMenu> orderMenus = createOrder.getMenuDtos()
+                .stream().map(m -> OrderMenu.createOrderMenu(menus.get(m.getMenuId()),m.getPrice(),m.getCount()))
+                .collect(Collectors.toList());
+        return orderRepository.save(Order.createOrder(user, orderMenus));
+    }
+
+    @Transactional
+    public boolean orderAccept(Long orderId){
+        Order order = this.findOne(orderId);
+        return order.ok();
+    }
+
+    @Transactional
+    public boolean orderComp(Long orderId){
+        Order order = this.findOne(orderId);
+        return order.complete();
+    }
+
+    @Transactional
+    public boolean orderCancle(Long orderId){
+        Order order = this.findOne(orderId);
+        return order.cancle();
+    }
 
 
+    private Map<Long,Menu> getOrderMenus(OrderRequestDto.CreateOrder createOrder){
+        Cafe cafe = cafeService.findById(createOrder.getCafeId());
+        Map<Long, Menu> menuMap = new HashMap<>();
+        for (Menu menu : cafe.getMenus()) {
+            menuMap.put(menu.getId(), menu);
+        }
+
+        Map<Long, Menu> resMenus = new HashMap<>();
+        for (OrderRequestDto.MenuDto menuDto : createOrder.getMenuDtos()) {
+            Long key = menuDto.getMenuId();
+            if(menuMap.containsKey(key)){
+                resMenus.put(key, menuMap.get(key));
+            }
+        }
+        return resMenus;
+    }
+
+    public Order findOne(Long id){
+        return orderRepository.findById(id).orElseThrow(()-> new OrderNotFoundException("Order not found."));
+    }
 }
